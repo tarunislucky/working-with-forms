65c2c70dcd8cecd1bcbc6e64bf5d75dbd70c8597
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-06-28 10:57:13 +0530

    Handling the "was touched" State

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,28 +2,31 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [nameInput, setNameInput] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
 
   const formSubmitHandler = event => {
     event.preventDefault();
-
+    setEnteredNameIsTouched(true);
     if (nameInput.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
     setEnteredNameIsValid(true);
+    console.log(nameInput);
   }
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
   const nameInputChangeHandler = event => {
     setNameInput(event.target.value);
   }
 
-  const nameInputClasses = enteredNameIsValid ? "form-control" : "form-control invalid";
+  const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control ";
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input onChange={nameInputChangeHandler} type='text' id='name' value={nameInput} />
-        {!enteredNameIsValid && <p className="error-text">Name must not be empty</p>}
+        {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
