04592476db70a34c7f1748885a63909f49c4b2f8
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-06-28 11:31:28 +0530

    react to lost to focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -13,19 +13,27 @@ const SimpleInput = (props) => {
       return;
     }
     setEnteredNameIsValid(true);
-    console.log(nameInput);
   }
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
   const nameInputChangeHandler = event => {
     setNameInput(event.target.value);
   }
+  const nameInputBlurHandler = event => {
+    setEnteredNameIsTouched(true);
+    if (nameInput.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  }
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched;
+
 
   const nameInputClasses = nameInputIsInvalid ? "form-control invalid" : "form-control ";
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input onChange={nameInputChangeHandler} type='text' id='name' value={nameInput} />
+        <input onChange={nameInputChangeHandler} onBlur={nameInputBlurHandler} type='text' id='name' value={nameInput} />
         {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
